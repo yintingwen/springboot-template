98c08502121e9055bb0fb3b5ffffad2d99ecdfa0
yintingwen <yintingwen@foxmail.com>
2024-10-15 18:16:54 +0800

    init

---
+++ src/test/java/com/yintingwen/springboot/ScriptDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yintingwen.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
